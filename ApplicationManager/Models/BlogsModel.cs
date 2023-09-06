a83d789f54046a251c3d2b0720afec1954822f19
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-09-06 15:53:56 +0500

    корр. страница "блог"

--- ApplicationManager/Models/BlogsModel.cs
+++ ApplicationManager/Models/BlogsModel.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ApplicationManager.Models
 {
-    public class BlogsModel :PageModel
+    public class BlogsModel : PageModel
     {
         public IQueryable<Blog> Blogs { get; set; }
         public string Name_page { get; set; }
