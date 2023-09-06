b16148788a2f1cf22e973023625100fd85042e61
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-09-06 15:51:20 +0500

    Вывод динамических названий страниц в компоненте

--- ApplicationManager/ViewComponents/HeaderButtViewViewComponent.cs
+++ ApplicationManager/ViewComponents/HeaderButtViewViewComponent.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationManager.Data;
+using ApplicationManager.Entitys;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApplicationManager.ViewComponents
 {
     public class HeaderButtViewViewComponent : ViewComponent
     {
+        private readonly IAppData data;
+        public HeaderButtViewViewComponent(IAppData data)
+        {
+            this.data = data;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            //осталось решить, как тут же в компонент (или уже в другой компонент) выгружать блок с описанием,
+            //это рандомные фразочки
+            IQueryable<MainPage> mainPages = data.GetMains().Where(item => item.Id >= 1 && item.Id <= 5);
+            return View("Default", mainPages);
         }
     }
 }
