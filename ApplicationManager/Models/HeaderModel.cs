8033c85142ca8df5cda381387ed894e07d3de69c
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-09-06 16:25:25 +0500

    подгрузка случайных заголовков в header

---
+++ ApplicationManager/Models/HeaderModel.cs
@@ -0,0 +1,10 @@
+﻿using ApplicationManager.Entitys;
+
+namespace ApplicationManager.Models
+{
+    public class HeaderModel
+    {
+        public string  Title { get; set; }
+        public IQueryable<MainPage> MainPages { get; set; }
+    }
+}
--- ApplicationManager/ViewComponents/HeaderButtViewViewComponent.cs
+++ ApplicationManager/ViewComponents/HeaderButtViewViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationManager.Data;
 using ApplicationManager.Entitys;
+using ApplicationManager.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApplicationManager.ViewComponents
@@ -15,8 +16,17 @@ namespace ApplicationManager.ViewComponents
         {
             //осталось решить, как тут же в компонент (или уже в другой компонент) выгружать блок с описанием,
             //это рандомные фразочки
-            IQueryable<MainPage> mainPages = data.GetMains().Where(item => item.Id >= 1 && item.Id <= 5);
-            return View("Default", mainPages);
+            Random r = new Random();
+            List<MainPage> titles = data.GetMains().Where(i => i.Id > 9).ToList();
+            int randIndex = r.Next(0, titles.Count());
+            HeaderModel model = new HeaderModel()
+            {
+                Title = titles[randIndex].Value,
+                MainPages = data.GetMains().Where(item => item.Id >= 1 && item.Id <= 5)
+            };
+           
+
+            return View("Default", model);
         }
     }
 }
